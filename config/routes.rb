8108197c1614974d033f24fdc0ac54010447c668
Thato Semoko <thatosmk@gmail.com>
2018-08-01 10:41:18 +0200

    added devise user model and site controller to serve standard pages, added username to user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'site/about'
+  get 'site/contact'
+  devise_for :users
+
+  # setup root page
+  root to: "site#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
