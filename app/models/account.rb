8d1cadfe2aa3c4d9dd8510d5f3d04892a5ee46b1
Thato Semoko <thatosmk@gmail.com>
2018-08-04 00:08:19 +0200

    added admin role to users

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,5 +4,4 @@ class Account < ApplicationRecord
   belongs_to :user
   has_many   :bookings
 
-  has_one_attached :avatar
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_205228) do
+ActiveRecord::Schema.define(version: 2018_08_03_220024) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2018_08_03_205228) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
